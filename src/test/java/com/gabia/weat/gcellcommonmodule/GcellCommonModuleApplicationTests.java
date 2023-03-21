de8096e70d1713f4c51c32eae9cccf2521fed650
tobi <tygwak@gabia.com>
2023-03-21 14:33:31 +0900

    Initial commit

---
+++ src/test/java/com/gabia/weat/gcellcommonmodule/GcellCommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.weat.gcellcommonmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcellCommonModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
